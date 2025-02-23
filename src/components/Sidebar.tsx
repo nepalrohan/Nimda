d6a45e3778cccc3e215147aa893241b39f57e034
nepalrohan <rohannepal098@gmail.com>
2025-02-23 13:53:13 +0545

    fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,6 @@
-import React, { ReactNode } from 'react';
-
-interface CommandProps {
-  children: ReactNode;
-  className?: string;
-}
-
-
-
-
-interface CustomCommandProps {
-  children: ReactNode;
-  className?: string;
-}
-export const Command = ({ children, className }: CommandProps) => {
-  return <div className={className}>{children}</div>;
-}; 
-
-
-export const CustomCommand = ({ children, className }: CustomCommandProps) => {
-  return <div className={className}>{children}</div>;
-};
-
 import {
-  Command as UICommand,
+  Command,
+  CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
@@ -30,53 +8,58 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from "@/components/ui/command";
+} from '@/components/ui/command';
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from 'lucide-react';
 import Link from 'next/link';
-import { LayoutDashboard, Newspaper, Folders, CreditCard, Settings, User } from "lucide-react";
 
-function Sidebar() {
+const Sidebar = () => {
   return (
-    <div className='h-full'>
-      <UICommand className='bg-secondary rounded-none'>
-        <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <LayoutDashboard className='h-4 w-4 mr-2' />
-              <Link href="/">Dashboard</Link>
-            </CommandItem>
-            <CommandItem>
-              <Newspaper className='h-4 w-4 mr-2' />
-              <Link href="/posts">Posts</Link>
-            </CommandItem>
-            <CommandItem>
-              <Folders className='h-4 w-4 mr-2' />
-              <Link href="/categories">Categories</Link>
-            </CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="Settings">
-            <CommandItem>
-              <User className='h-4 w-4 mr-2' />
-              <span>Profile</span>
-              <CommandShortcut>⌘P</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <CreditCard className='h-4 w-4 mr-2' />
-              <span>Billing</span>
-              <CommandShortcut>⌘B</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-              <Settings className='h-4 w-4 mr-2' />
-              <span>Setting</span>
-              <CommandShortcut>⌘S</CommandShortcut>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </UICommand>
-    </div>
+    <Command className='bg-secondary rounded-none'>
+      <CommandInput placeholder='Type a command or search...' />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading='Suggestions'>
+          <CommandItem>
+            <LayoutDashboard className='mr-2 h-4 w-4' />
+            <Link href='/'>Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Newspaper className='mr-2 h-4 w-4' />
+            <Link href='/posts'>Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className='mr-2 h-4 w-4' />
+            <Link href='#'>Categories</Link>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading='Settings'>
+          <CommandItem>
+            <User className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className='mr-2 h-4 w-4' />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
   );
-}
+};
 
 export default Sidebar;
